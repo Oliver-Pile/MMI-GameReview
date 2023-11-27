af997dcab5425aadd9c08c7c3e5c1d98c73b1579
Oliver Pile <oliver.pile@bt.com>
2023-11-27 13:31:58 +0000

    Display games from db for genre

--- pages/games.php
+++ pages/games.php
@@ -10,4 +10,22 @@
 <div class="container">
   <h1><?php echo $current_genre["name"]?> Games</h1>
 
+  <div class="row">
+    <?php foreach($games as $game) { ?>
+      <div class="col-md-12 card">
+        <div class="row game-row">
+        <div class="col-md-3">
+          <img class="card-image" src="images/genre-images/fps_image.jpeg"/>
+        </div>
+        <div class="col-md-9">
+          <div class="d-inline-block">
+            <div class="card-body">
+              <h3 class="card-title"><?php echo $game["title"] ?></h3>
+              <p class="card-text"> <?php echo $game["description"] ?> </p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    <?php } ?>
 </div>
--- pages/genres.php
+++ pages/genres.php
@@ -10,7 +10,7 @@
     <div class="col-lg-6">
       <a class="card card-body" href="index.php?p=games&genre_id=<?php echo $genre["id"]; ?>" >
         <div class = "d-flex">
-          <div class="genre-card-image" style="background-image: url('./images/genre-images/<?php echo $genre['image']; ?>');">
+          <div class="card-image" style="background-image: url('./images/genre-images/<?php echo $genre['image']; ?>');">
           </div>
           <div class="genre-title"><h3><?php echo $genre['name']; ?></h3></div>
         </div>
