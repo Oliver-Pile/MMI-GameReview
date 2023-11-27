45590fd676bb9ac6378b0a65c9777da27d6100d6
Oliver Pile <oliver.pile@bt.com>
2023-11-27 10:23:29 +0000

    Fix typo in seeds

--- includes/database/seeds.sql
+++ includes/database/seeds.sql
@@ -1,8 +1,9 @@
 USE GameReview;
 
+
 /* Insert test data for the Genre table */ 
 INSERT INTO Genre (name, image) VALUES("FPS", "fps_image.jpeg");
-INSERT INTO Genre (name) VALUES("Open-World", "gta_5_image.jpeg");
-INSERT INTO Genre (name) VALUES("Simulation");
-INSERT INTO Genre (name) VALUES("Horror");
-INSERT INTO Genre (name) VALUES("Indie");
+INSERT INTO Genre (name, image) VALUES("Open-World", "open_world_image.jpeg");
+INSERT INTO Genre (name, image) VALUES("Simulation", "simulation_image.jpeg");
+INSERT INTO Genre (name, image) VALUES("Horror", "horror_image.jpeg");
+INSERT INTO Genre (name, image) VALUES("Indie", "indie_image.jpeg");
