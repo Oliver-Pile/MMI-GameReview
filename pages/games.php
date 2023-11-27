494e878ee6f95e7cdbbff9792f04e84ba8365786
Oliver Pile <oliver.pile@bt.com>
2023-11-27 15:07:49 +0000

    Link from games to individual game

--- pages/games.php
+++ pages/games.php
@@ -7,24 +7,24 @@
 ?>
 
 
-<div class="container">
+<div class="container" id="games-for-genre-page">
   <h1><?php echo $current_genre["name"]?> Games</h1>
 
   <div class="row">
     <?php foreach($games as $game) { ?>
       <div class="col-md-12 card">
         <div class="row game-row">
-        <div class="col-md-3">
-          <img class="card-image" src="images/game-images/<?php echo $game["image"]; ?>"/>
-        </div>
-        <div class="col-md-9">
-          <div class="d-inline-block">
-            <div class="card-body">
-              <h3 class="card-title"><?php echo $game["title"] ?></h3>
-              <p class="card-text"> <?php echo $game["description"] ?> </p>
-            </div>
+          <div class="col-md-3">
+            <img class="card-image" src="images/game-images/<?php echo $game["image"]; ?>"/>
           </div>
-        </div>
+          <a class="col-md-9" href="index.php?p=game&id=<?php echo $game["id"]; ?>">
+            <div class="d-inline-block">
+              <div class="card-body">
+                <h3 class="card-title"><?php echo $game["title"] ?></h3>
+                <p class="card-text"> <?php echo $game["description"] ?> </p>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     <?php } ?>
