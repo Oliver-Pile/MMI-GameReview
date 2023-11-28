fb0227f6286404539efd573bc337b0bb82534725
Oliver Pile <oliver.pile@bt.com>
2023-11-28 10:30:23 +0000

    Add validation for login/register

---
+++ pages/helpers/login_helper.php
@@ -0,0 +1,88 @@
+<?php
+  if($_POST['reg']) {
+    $email = htmlspecialchars($_POST["email"]);
+    $username = htmlspecialchars($_POST["username"]);
+    $password = htmlspecialchars($_POST["password"]);
+    $confirm_password = htmlspecialchars($_POST["password_confirm"]);
+
+    $errors = validateReg($email, $username, $password, $confirm_password);
+
+    if ($errors) {
+      displayErrors($errors);
+    }
+  } else if($_POST['login']) {
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
+
+    $errors = validateLogin($email, $password);
+
+    if ($errors) {
+      displayErrors($errors);
+    }
+  }
+
+  function displayErrors($errors) {
+    echo '<div class="alert alert-danger" role="alert">';
+      echo join("<br>", $errors);
+    echo '</div>';
+  }
+
+  function validateReg($email, $username, $password, $confirm_password) {
+    $errors = array();
+    array_push($errors, validateEmail($email));
+    array_push($errors, validateUsername($username));
+    array_push($errors, validatePasswordSet($password, "Password"));
+    array_push($errors, validatePasswordSet($confirm_password, "Password Confirmation"));
+    array_push($errors, validatePasswordsMatch($password, $confirm_password));
+
+    return array_filter($errors);
+  }
+
+  function validateLogin($email, $password) {
+    $errors = array();
+    array_push($errors, validateEmail($email));
+    array_push($errors, validatePasswordSet($password, "Password"));
+
+    return array_filter($errors);
+  }
+
+  function validateEmail($email) {
+    if ($email == "") {
+      $error = "Email not set";
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $error = "Not a valid email address";
+    } else {
+      $error = null;
+    }
+    return $error;
+  }
+
+  function validateUsername($username) {
+    if ($username == "") {
+      $error = "Username not set";
+    } else {
+      $error = null;
+    }
+    return $error;
+  }
+
+  function validatePasswordSet($password, $type) {
+    if ($password == "") {
+      $error = "$type not set";
+    } else if (strlen($password) < 8) {
+      $error = "$type must be longer than 8 characters";
+    } else {
+      $error = null;
+    }
+    return $error;
+  }
+
+  function validatePasswordsMatch($password, $confirm_password) {
+    if ($password !== $confirm_password) {
+      $error = "Passwords do not match";
+    } else {
+      $error = null;
+    }
+    return $error;
+  }
+?>
---
+++ pages/login.php
@@ -0,0 +1,44 @@
+<?php 
+  require_once(__DIR__ . '/helpers/login_helper.php')
+?>
+
+<div class="container">
+  <h1 class="mb-4 pb-2">Login or Register</h1>
+  <div class="row">
+    <div class="col-md-3">
+      <form id="login-form" method="post" action="">
+        <div class="form-group">
+          <label for="login_email">Email address</label>
+          <input type="email" class="form-control" id="login_email" name="email">
+        </div>
+        <div class="form-group">
+          <label for="login_password">Password</label>
+          <input type="password" class="form-control" id="login_password" name="password">
+        </div>
+        <button type="submit" name="login" value="1" class="btn btn-primary">Login</button>
+      </form>
+    </div>
+    <div class="col-md-3">
+      <form id="registration-form" method="post" action="">
+        <div class="form-group">
+          <label for="reg_email">Email address</label>
+          <input type="email" class="form-control" id="reg_email" name="email">
+        </div>
+        <div class="form-group">
+          <label for="reg_username">Username</label>
+          <input type="text" class="form-control" id="reg_username" name="username">
+        </div>
+        <div class="form-group">
+          <label for="reg_password">Password</label>
+          <input type="password" class="form-control" id="reg_password" name="password">
+        </div>
+        <div class="form-group">
+          <label for="reg_password_confirm">Confirm Password</label>
+          <input type="password" class="form-control" id="reg_password_confirm"
+          name="password_confirm">
+        </div>
+        <button type="submit" name="reg" value="1" class="btn btn-primary">Register</button>            
+      </form>
+    </div>
+  </div>
+</div>
