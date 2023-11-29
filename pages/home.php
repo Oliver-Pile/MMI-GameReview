cee8e8a3e0a8d328f7828dedb20078a680d542ef
Oliver Pile <oliver.pile@bt.com>
2023-11-29 16:53:00 +0000

    Add basic home layout

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,23 @@
+<!-- Make random game selector then update image/text -->
 <div class="container" id="home-page">
-  <h1> Game Review </h1>  
+  <h1> Game Review </h1> 
+  <div>
+    <div class="card home-info">
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <div class="featured-game-container">
+      <h2> Featured Game </h2>
+      <div class="featured-game">
+        <div class="game-image" style="background-image: url(images/game-images/gta5_image.jpeg)"></div>
+        <div class="card featured-game-text">
+          <h3 class="card-title">GTA 5</h3>
+          <p class="card-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,  
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
