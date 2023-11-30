1290b9856cf983731e3d8d3ec6f22aced135783d
Oliver Pile <oliver.pile@bt.com>
2023-11-30 11:38:38 +0000

    Allow alerts to be dismissed

--- pages/components/failure_alert.php
+++ pages/components/failure_alert.php
@@ -1,3 +1,4 @@
-<div class="alert alert-danger" role="alert">
+<div class="alert alert-danger alert-dismissible" role="alert">
   <?php echo $failure_message ?>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
--- pages/components/success_alert.php
+++ pages/components/success_alert.php
@@ -1,3 +1,4 @@
-<div class="alert alert-success" role="alert">
+<div class="alert alert-success alert-dismissible" role="alert">
   <?php echo $success_message ?>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
