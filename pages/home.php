43fc930dd8d8564f2c5427cb5e8e6348f594a1f6
Oliver Pile <oliver.pile@bt.com>
2023-12-14 10:21:40 +0000

    Fix formatting for home page

--- pages/home.php
+++ pages/home.php
@@ -1,23 +1,29 @@
 <!-- Make random game selector then update image/text -->
 <div class="container" id="home-page">
   <h1> Game Review </h1> 
-  <div>
-    <div class="card home-info">
+  <div class="row">
+  <div class="col-md-3"></div>
+    <div class="card col-md-6">
       <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Welcome to the game review site. You can view different genres and games or search to find a specific game.<br>
+        For a specific game, you can view its raiting and any reviews created for it.<br>
+        If you create an account you may bookmark games which will become visible from your account as well as create a new review.
       </p>
     </div>
-    <div class="featured-game-container">
+    <div class="col-md-3"></div>
+  </div>
+    <div class="">
       <h2> Featured Game </h2>
-      <div class="featured-game">
-        <div class="game-image" style="background-image: url(images/game-images/gta5_image.jpeg)"></div>
-        <div class="card featured-game-text">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="game-image col-md-4" style="background-image: url(images/game-images/gta5_image.jpeg)"></div>
+        <div class="card featured-game-text col-md-4">
           <h3 class="card-title">GTA 5</h3>
           <p class="card-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,  
           </p>
         </div>
+        <div class="col-md-2"></div>
       </div>
     </div>
-  </div>
 </div>
